1b1b68b7861009066cd0a557e1d354b374c0a2bb
Anonymous Raccoon <32224410+AnonymusRaccoon@users.noreply.github.com>
2018-08-11 16:08:42 +0200

    Solving movement bugs.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,8 +24,6 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int smallJumpPush = 5;
     private bool groundedLastFrame = false;
     private int wallDirection;
-    private int wallJumpTimer = 0;
-    private bool wallJumped;
     private float jumpDirection;
     [HideInInspector] public float velocity = 0;
 
@@ -40,7 +38,6 @@ public class PlayerMovement : MonoBehaviour
         {
             if (groundedLastFrame)
             {
-                wallJumped = false;
                 return true;
             }
             else
@@ -55,18 +52,12 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsSliding()
     {
-        List<RaycastHit> raycastHits = Physics.BoxCastAll(rb.position - new Vector2(0.6f, 0), new Vector3(0.55f, 0, 1), Vector3.forward, Quaternion.identity, 1.2f).ToList();
-        List<RaycastHit> walls = new List<RaycastHit>();
-
-        foreach (RaycastHit hit in raycastHits)
-        {
-            walls.Add(hit);
-        }
+        List<RaycastHit2D> walls = Physics2D.RaycastAll(rb.position, Vector2.left, GetComponent<Renderer>().bounds.size.x / 2 + .1f).ToList();
+        walls.AddRange(Physics2D.RaycastAll(rb.position, Vector2.right, 5.8f));
+        walls.RemoveAll(x => x.collider.tag == "Player");
 
         if (walls.Count > 0)
         {
-            wallJumpTimer = 10;
-
             if (walls[0].transform.position.x > rb.position.x && Input.GetAxis(Horizontal) > 0)
             {
                 wallDirection = 1;
@@ -78,7 +69,6 @@ public class PlayerMovement : MonoBehaviour
                 return true;
             }
         }
-        wallJumpTimer--;
         return false;
     }
 
@@ -111,7 +101,6 @@ public class PlayerMovement : MonoBehaviour
         bool isGrounded = IsGrounded();
         bool isSliding = IsSliding();
         bool airControl = AirControl(isGrounded);
-        print(isSliding);
 
         if (-1 < velocity && velocity < 1)
             velocity = 0;
@@ -122,15 +111,6 @@ public class PlayerMovement : MonoBehaviour
             velocity += 0.5f;
 
         float horizontalVel = Input.GetAxis(Horizontal);
-
-        //Change air velocity after wall jump
-        if (wallJumped)
-        {
-            if (-0.3 < horizontalVel && horizontalVel < 0.3)
-                horizontalVel = 0.6f * jumpDirection;
-            if (Mathf.Sign(horizontalVel) == Mathf.Sign(horizontalVel))
-                horizontalVel /= 2;
-        }
 
         //Make player move
         if (!isSliding || (isSliding && Mathf.Sign(wallDirection) != Mathf.Sign(Input.GetAxis(Horizontal))))
@@ -147,12 +127,12 @@ public class PlayerMovement : MonoBehaviour
         //Make a small jump if user drop the button
         if (Input.GetKeyUp(JumpKey) && !isGrounded && rb.velocity.y > smallJump)
         {
-            if (wallJumped && Mathf.Abs(rb.velocity.x) > smallJumpPush)
-            {
-                velocity = smallJumpPush * jumpDirection;
-                rb.velocity = new Vector2(rb.velocity.x - (wallJumpPush - smallJumpPush) * jumpDirection, rb.velocity.y);
-            }
-            else
+            //if (Mathf.Abs(rb.velocity.x) > smallJumpPush)
+            //{
+            //    velocity = smallJumpPush * jumpDirection;
+            //    rb.velocity = new Vector2(rb.velocity.x - (wallJumpPush - smallJumpPush) * jumpDirection, rb.velocity.y);
+            //}
+            //else
                 rb.velocity = new Vector2(rb.velocity.x, smallJump);
         }
 
@@ -160,30 +140,8 @@ public class PlayerMovement : MonoBehaviour
         if (rb.velocity.y < 1 && !isSliding)
             rb.AddForce(new Vector3(0, gravity, 0), ForceMode2D.Force);
 
-        //WallSlide
-        if (wallJumpTimer > 0)
-        {
-            //Wall Jump
-            if (Input.GetKeyDown(JumpKey) && !isGrounded)
-            {
-                wallJumped = true;
-                jumpDirection = -wallDirection;
-                velocity = wallJumpPush * jumpDirection;
-                rb.AddForce(new Vector3(wallJumpPush * jumpDirection, wallJump, 0), ForceMode2D.Impulse);
-            }
-            else if (isSliding)
-                rb.AddForce(new Vector3(0, Mathf.Abs(rb.velocity.y) + gravity / 2, 0), ForceMode2D.Force);
-        }
-    }
-
-    Vector3 NormaliseMovement(float x, float y)
-    {
-        Vector3 input = new Vector3(x, y, 0);
-        input.Normalize();
-
-        if (input.x == 0 && input.y == 0)
-            input.x = 1;
-
-        return input;
+        //Wall Slide
+        if (isSliding && !isGrounded)
+            rb.AddForce(new Vector3(0, Mathf.Abs(rb.velocity.y) + gravity / 2, 0), ForceMode2D.Force);
     }
 }
