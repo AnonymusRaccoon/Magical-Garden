32bb303d7a801af6c03bd57b8ee96a6b7ad37a78
happy44300 <bilal.molli@gmail.com>
2018-08-12 22:41:39 +0200

    Le pokepush

---
+++ Assets/Pokedex.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Threading.Tasks;
+
+public class Pokedex : MonoBehaviour
+{
+    public TextMeshProUGUI PokeText;
+    public int PokeTemps = 10;
+    private bool PokeBool = false;
+
+    public async void PokeDescription(int PokeNumero)
+    {
+        if (PokeBool == false)
+        {
+            string PokeSubDescription = GetComponent<InventoryManager>().items[PokeNumero].description;
+            PokeText.text = null;
+            PokeBool = true;
+            for (int i = 0; i < PokeSubDescription.Length; i++)
+            {
+                PokeText.text += PokeSubDescription[i];
+                await Task.Delay(PokeTemps);
+            }
+            PokeBool = false;
+        }
+
+    }
+}
