d804bd7edc349df7430ffa69726576d0a6a9c1ee
Anonymous Raccoon <32224410+AnonymusRaccoon@users.noreply.github.com>
2018-08-14 12:22:50 +0200

    Adding logo and publishing the game !

--- Assets/Pokedex.cs
+++ Assets/Pokedex.cs
@@ -11,24 +11,28 @@ public class Pokedex : MonoBehaviour
     private bool ArretDéfilement = false;
     private string PokeSubDescription;
 
-    public async void PokeDescription(int PokeNumero)
+    //Remove comments for windows pokedefilement
+    public /*async*/ void PokeDescription(int PokeNumero)
     {
-        ArretDéfilement = false;
         PokeSubDescription = GetComponent<InventoryManager>().items[PokeNumero].description;
-        PokeText.text = null;
-        for (int i = 0; i < PokeSubDescription.Length; i++)
-        {
-            if (ArretDéfilement == true)
-            {
-                return;
-            }
-            PokeText.text += PokeSubDescription[i];
-            await Task.Delay(PokeTemps);
-        }
+        string descriptionSave = PokeSubDescription;
+        PokeText.text = PokeSubDescription; //null
+
+        //if (PokeSubDescription != descriptionSave)
+        //    return;
+
+        //for (int i = 0; i < PokeSubDescription.Length; i++)
+        //{
+        //    if (PokeSubDescription != descriptionSave)
+        //        return;
+
+        //    PokeText.text += PokeSubDescription[i];
+        //    await Task.Delay(PokeTemps);
+        //}
     }
     public void UpdateMissionText()
     {
-        ArretDéfilement = true;
-        PokeText.text = GetComponent<Mission>().GetMissionText();
+        PokeSubDescription = GetComponent<Mission>().GetMissionText();
+        PokeText.text = PokeSubDescription;
     }
 }
